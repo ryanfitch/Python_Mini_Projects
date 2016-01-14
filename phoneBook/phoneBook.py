6fa98db6c6c9ded9f5fb1e0bebb7c8de2fa6a8ae
Ryan Fitch <ryanfitch@mac.com>
2016-01-13 16:50:53 -0800

    second commit

--- phoneBook/phoneBook.py
+++ phoneBook/phoneBook.py
@@ -2,67 +2,107 @@
 # Python 3.5.  Phone / contact app.
 # A simple phone book app made with Python and tkinter for saving and browsing contacts.
 
-
 from tkinter import *
 from tkinter import ttk
 
-root=Tk()
+phonelist = [
+  ['Meyers, Chris',  '343-4349'],
+  ['Smith, Robert',  '689-1234'],
+  ['Jones, Janet',   '483-5432'],
+  ['Barnhart, Ralph','683-2341'],
+  ['Nelson, Eric',   '485-2689'],
+  ['Prefect, Ford',  '987-6543'],
+  ['Zigler, Mary',   '567-8901'],
+  ['Smith, Bob',     '689-1234']
+]
 
-# Phone Book title
-label = ttk.Label(root, text = "     Phone Book")
-label.pack(fill = BOTH, expand = True, padx=10, pady=10)
-label.config(foreground = 'black', background = '#e9e9e9')
-label.config(font = ('Tahoma', 35, 'bold'))
+def whichSelected (self):
+        print ("At {} of {}".format(select.curselection(), len(phonelist))
+        return int(select.curselection()[0])
 
-# sets up panedwindow frames
-panedwindow = ttk.Panedwindow(root, orient = VERTICAL)
-panedwindow.pack(fill = BOTH, expand = True)
+def addEntry () :
+    phonelist.append ([nameVar.get(), phoneVar.get()])
+    setSelect ()
 
-frame1 = ttk.Frame(panedwindow, width = 350, height = 0, relief = GROOVE)
-frame2 = ttk.Frame(panedwindow, width = 350, height = 50, relief = GROOVE)
-frame3 = ttk.Frame(panedwindow, width = 350, height = 200, relief = SUNKEN)
+def updateEntry() :
+    phonelist[whichSelected()] = [nameVar.get(), phoneVar.get()]
+    setSelect ()
 
-panedwindow.add(frame1, weight = 1)
-panedwindow.add(frame2, weight = 1)
-panedwindow.add(frame3, weight = 2)
+def deleteEntry() :
+    del phonelist[whichSelected()]
+    setSelect ()
 
-# panel/frame2 Name:/Number: area
-panedwindow2 = ttk.Panedwindow(frame2, orient = HORIZONTAL)
-panedwindow2.pack(fill = BOTH, expand = True, padx=50, pady=10)
+def loadEntry  () :
+    name, phone = phonelist[whichSelected()]
+    nameVar.set(name)
+    phoneVar.set(phone)
 
-frameLeft = ttk.Frame(panedwindow2, width = 100, height = 75, relief = SUNKEN)
-frameRight = ttk.Frame(panedwindow2, width = 250, height = 75, relief = SUNKEN)
+def makeWindow () :
+    global nameVar, phoneVar, select
+    win=Tk()
 
-panedwindow2.add(frameLeft, weight = 1)
-panedwindow2.add(frameRight, weight = 2)
+    # Phone Book title
+    label = ttk.Label(win, text = "     Phone Book")
+    label.pack(fill = BOTH, expand = True, padx=10, pady=10)
+    label.config(foreground = 'black', background = '#e9e9e9')
+    label.config(font = ('Tahoma', 35, 'bold'))
 
-labelName = ttk.Label(frameLeft, text = "Name:")
-labelNumber = ttk.Label(frameLeft, text = "Number:")
-labelName.pack(fill = BOTH, expand = True)
-labelNumber.pack(fill = BOTH, expand = True)
+    # sets up panedwindow frames
+    panedwindow = ttk.Panedwindow(win, orient = VERTICAL)
+    panedwindow.pack(fill = BOTH, expand = True)
 
-nameEntry = ttk.Entry(frameRight, width = 15)
-nameEntry.pack(fill = BOTH, expand = True)
-phoneEntry = ttk.Entry(frameRight, width = 15)
-phoneEntry.pack(fill = BOTH, expand = True)
+    frame1 = ttk.Frame(panedwindow, width = 350, height = 0, relief = GROOVE)
+    frame2 = ttk.Frame(panedwindow, width = 350, height = 50, relief = GROOVE)
+    frame3 = ttk.Frame(panedwindow, width = 350, height = 200, relief = SUNKEN)
 
-b1 = Button(frame2, text = "Add")
-b2 = Button(frame2, text = "Update")
-b3 = Button(frame2, text = "Delete")
-b4 = Button(frame2, text = "Load")
+    panedwindow.add(frame1, weight = 1)
+    panedwindow.add(frame2, weight = 1)
+    panedwindow.add(frame3, weight = 2)
 
-b1.pack(side=LEFT, padx=10, pady=5)
-b2.pack(side=LEFT, padx=10, pady=5)
-b3.pack(side=LEFT, padx=10, pady=5)
-b4.pack(side=LEFT, padx=10, pady=5)
+    # panel/frame2 Name:/Number: area
+    panedwindow2 = ttk.Panedwindow(frame2, orient = HORIZONTAL)
+    panedwindow2.pack(fill = BOTH, expand = True, padx=50, pady=10)
 
-logo = PhotoImage(file = 'phone.gif') # change path to image as necessary
-label.config(image = logo)
-# sets which image and text is displayed
-label.config(compound = 'text')
-label.config(compound = 'left')
+    frameLeft = ttk.Frame(panedwindow2, width = 100, height = 75, relief = SUNKEN)
+    frameRight = ttk.Frame(panedwindow2, width = 250, height = 75, relief = SUNKEN)
 
-label.img = logo
-label.config(image = label.img)
+    panedwindow2.add(frameLeft, weight = 1)
+    panedwindow2.add(frameRight, weight = 2)
 
-root.mainloop()
+    labelName = ttk.Label(frameLeft, text = "Name:")
+    nameVar= StringVar()
+    labelName.pack(fill = BOTH, expand = True)
+               
+    labelNumber = ttk.Label(frameLeft, text = "Number:")
+    phoneVar= StringVar()
+    labelNumber.pack(fill = BOTH, expand = True)
+
+    nameEntry = ttk.Entry(frameRight, width = 15)
+    nameEntry.pack(fill = BOTH, expand = True)
+    phoneEntry = ttk.Entry(frameRight, width = 15)
+    phoneEntry.pack(fill = BOTH, expand = True)
+
+    b1 = Button(frame2, text = " Add  ", command=addEntry)
+    b2 = Button(frame2, text = "Update", command=updateEntry)
+    b3 = Button(frame2, text = "Delete", command=deleteEntry)
+    b4 = Button(frame2, text = " Load ", command=loadEntry)
+
+    b1.pack(side=LEFT, padx=10, pady=5)
+    b2.pack(side=LEFT, padx=10, pady=5)
+    b3.pack(side=LEFT, padx=10, pady=5)
+    b4.pack(side=LEFT, padx=10, pady=5)
+
+    
+
+    logo = PhotoImage(file = 'phone.gif') 
+    label.config(image = logo)
+    # sets which image and text is displayed
+    label.config(compound = 'text')
+    label.config(compound = 'left')
+
+    label.img = logo
+    label.config(image = label.img)
+
+win = makeWindow()
+setSelect()
+win.mainloop()
---
+++ phoneBook/phoneBook2.py
@@ -0,0 +1,100 @@
+# phoneBook2.py by Ryan Fitch [http://ryan-fitch.com/]
+# Python 3.5.  Phone / contact app.
+# A simple phone book app made with Python and tkinter for saving and browsing contacts.
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+phonelist = [
+  ['Meyers, Chris',  '343-4349'],
+  ['Smith, Robert',  '689-1234'],
+  ['Jones, Janet',   '483-5432'],
+  ['Barnhart, Ralph','683-2341'],
+  ['Nelson, Eric',   '485-2689'],
+  ['Prefect, Ford',  '987-6543'],
+  ['Zigler, Mary',   '567-8901'],
+  ['Smith, Bob',     '689-1234']
+]
+
+# for i in phonelist:
+#    print(phonelist[i])
+
+class Phonebook:
+
+    def __init__(self, master):
+
+        master.title('Phone Book')
+        master.resizable(False, False)
+        master.configure(background = '#e9e9e9')
+
+        self.style = ttk.Style()
+        self.style.configure('TFrame', background = '#e9e9e9')
+        self.style.configure('TButton', background = '#e9e9e9')
+        self.style.configure('TLabel', background = '#e9e9e9', font = ('Tahoma', 14))
+        self.style.configure('Header.TLabel', font = ('Tahoma', 40, 'bold'))
+
+        self.frame_header = ttk.Frame(master)
+        self.frame_header.pack()
+
+        self.logo = PhotoImage(file = 'phone.gif')
+        ttk.Label(self.frame_header, image = self.logo).grid(row = 0, column = 0, columnspan = 1, padx = 15, pady = 15)
+        ttk.Label(self.frame_header, text = "Phone Book", style = 'Header.TLabel').grid(row = 0, column = 2, columnspan = 3, padx = 15, pady = 15)
+
+        self.frame_content = ttk.Frame(master)
+        self.frame_content.pack()
+
+        ttk.Label(self.frame_content, text = 'Name:').grid(row = 0, column = 1, padx = 5, sticky = 'e')
+        ttk.Label(self.frame_content, text = 'Number:').grid(row = 1, column = 1, padx = 5, sticky = 'e')
+        # ttk.Label(self.frame_content, text = 'Notes:').grid(row = 2, column = 1, padx = 5, sticky = 'e')
+
+        self.entry_name = ttk.Entry(self.frame_content, width = 12)
+        self.entry_number = ttk.Entry(self.frame_content, width = 12)
+        # self.entry_notes = ttk.Entry(self.frame_content, width = 12)
+        self.text_addressBook = Text(self.frame_content, width = 60, height = 20, font = ('Tahoma', 12))
+
+        self.entry_name.grid(row = 0, column = 2, padx = 5)
+        self.entry_number.grid(row = 1, column = 2, padx = 5)
+        # self.entry_notes.grid(row = 2, column = 2, padx = 5)
+        self.text_addressBook.grid(row = 5, column = 0, columnspan = 4, padx = 0)
+        self.text_addressBook.insert(INSERT, phonelist)
+
+        ttk.Button(self.frame_content, text = 'Add', command = self.add).grid(row = 4, column = 0, padx = 5, pady = 10)
+        ttk.Button(self.frame_content, text = 'Update').grid(row = 4, column = 1, padx = 5, pady = 10)
+        ttk.Button(self.frame_content, text = 'Delete', command = self.clear).grid(row = 4, column = 2, padx = 5, pady = 10)
+        ttk.Button(self.frame_content, text = 'Find').grid(row = 4, column = 3, padx = 5, pady = 10)
+
+
+    def whichSelected (self):
+        # print ("At {} of {}".format(select.curselection(), len(phonelist))
+        # return int(select.curselection()[0])
+        pass
+               
+    def add(self):
+        # phonelist.append([nameVar(), phoneVar.get()])
+        phonelist.append([self.entry_name.get(), self.entry_number.get()])
+        # print('Name: {}'.format(self.entry_name.get()))
+        # print('Number: {}'.format(self.entry_number.get()))
+        # print('Notes: {}'.format(self.entry_notes.get()))
+        # self.text_addressBook.clear()
+        messagebox.showinfo(title = 'Add Feedback', message = 'Entry added!')
+        print(phonelist)
+        self.text_addressBook.insert(INSERT, phonelist)
+        
+
+    def clear(self):
+        self.entry_name.delete(0, 'end')
+        self.entry_number.delete(0, 'end')
+        self.entry_notes.delete(0, 'end')
+
+    
+
+
+def main ():
+
+    root = Tk()
+    phonebook = Phonebook(root)
+    root.mainloop()
+
+if __name__=="__main__": main()
+        
