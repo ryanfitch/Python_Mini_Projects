1563bed7bc7068a320df57074e7fe41fb63943d3
Ryan Fitch <ryanfitch@mac.com>
2016-01-17 18:55:58 -0800

    A simple application for checking to see if certain office locations are open.

--- officeTime.py
+++ officeTime.py
@@ -2,37 +2,53 @@
 # Python 2.7.  App to show if the office is open in a few different locations.
 # A simple application for checking to see if certain office locations are open.
 
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime
 
 # grabbing greenwhich meantime hour and calculating other time zones for Portland & NYC and saving them to variables
 # also converting time as an integer so that it can be evaluated correctly
 gmHour = int(strftime("%H", gmtime()))
-portHour = gmHour - 8
-nyHour = gmHour - 5
 # grabbing greenwhich minutes and saving it to a variable
 gmMinute = int(strftime("%M", gmtime()))
+# grabbing local portland hour and saving it to a variable
+portHour = int(strftime("%H"))
 
-# defines main function to see if the time passed in is within office hours and also converts it from military time.
+# sets the nyc military hour
+def nyChange (portHour):
+    if (portHour < 21):
+        nyHour = portHour + 3
+        return nyHour
+    else:
+        nyHour = portHour - 12
+        return nyHour
+
+nyHour = nyChange(portHour)
+
+# defines main function to see if the time passed-in is within office hours and also converts it from military time.
 def isOpen (curTime):
-    # checks to see if time passed in is within office hours
-    if (curTime >= 9 and curTime <= 21):
+    # checks to see if time passed-in is within office hours
+    if (curTime >= 9 and curTime <= 20):
         office = "is open"
     else:
         office = "is closed"
-    # checks if time passed in is above 12 hrs (military time) and then converts it to normal time if so.
-    if (curTime > 12):
-        amPm = "PM"
-        normHour = curTime - 12
-    else:
-        amPm = "AM"
-        normHour = curTime
-    # checks which time is passed in and sets location
+    # checks which time is passed-in and sets location
     if curTime is gmHour:
         location = "London"
+        amPm = strftime("%p", gmtime())
     elif curTime is portHour:
         location = "Portland"
+        amPm = strftime("%p")
     else:
-        location = "New York City"
+        if curTime >= 3: 
+            location = "New York City"
+            amPm = strftime("%p")
+        else:
+            location = "New York City"
+            amPm = "AM"
+    # checks if time passed-in is above 12 hrs (military time) and then converts it to normal time if so.
+    if (curTime > 12):
+        normHour = curTime - 12
+    else:
+        normHour = curTime
     # fixes minute formatting problem where there is no '0' added with single digit minutes
     if gmMinute < 10:
         minute = '0'
