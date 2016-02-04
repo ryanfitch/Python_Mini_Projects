c7f36c7fb85e3260776e5bda427b6c1f915a0446
Ryan Fitch <ryanfitch@mac.com>
2016-02-04 14:59:22 -0800

    Working through the W3 exercises

---
+++ Python-W3-Exercises/python-exercise30.py
@@ -0,0 +1,9 @@
+# Write a Python program that will accept the base and height of a triangle
+# and compute the area.
+
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+
+area = b*h/2
+
+print("Area = ", area)
