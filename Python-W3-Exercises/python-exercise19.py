90b0e51120878101d63178a79af8abc31d0152c8
Ryan Fitch <ryanfitch@mac.com>
2016-02-04 13:44:51 -0800

    Working through the W3 exercises

---
+++ Python-W3-Exercises/python-exercise19.py
@@ -0,0 +1,11 @@
+# Write a Python program to get a new string from a given string where
+# "Is" has been added to the front. If the given string already begins with
+# "Is" then return the string unchanged.
+
+str = input("Give me a sentence: ")
+strSplit = str.split(" ")
+if str[0] == "Is":
+    print(str)
+else:
+    new = str.lower()
+    print("Is " + new)
