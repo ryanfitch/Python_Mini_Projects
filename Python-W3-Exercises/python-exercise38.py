8b3d70c4690f39f47ba8661640b8f26fbd7bce1d
Ryan Fitch <ryanfitch@mac.com>
2016-02-04 15:51:45 -0800

    Working through the W3 exercises

---
+++ Python-W3-Exercises/python-exercise38.py
@@ -0,0 +1,7 @@
+# Write a Python program to solve (x + y) * (x + y).
+
+x = int(input("Give me a number: "))
+y = int(input("Give me another number: "))
+z = (x + y) * (x + y)
+
+print ("The answer is: {}".format(z))
