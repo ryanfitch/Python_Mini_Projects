d0ab88e41bb5eb8847e5dc97d61ff1eb0b1f73c7
Ryan Fitch <ryanfitch@mac.com>
2016-02-03 16:57:21 -0800

    Working through the W3 exercises

---
+++ Python-W3-Exercises/python-exercise8.py
@@ -0,0 +1,7 @@
+# Write a Python program to display the first and last colors
+# from the following list.
+
+color_list = ["Red","Green","White" ,"Black"]
+
+print ("Full color list = {}".format(color_list))
+print ("First and last colors in the list are = {}, {}".format(color_list[0], color_list[3]))
