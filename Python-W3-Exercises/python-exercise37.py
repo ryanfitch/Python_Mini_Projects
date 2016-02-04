dbc6aeb6b20b65077b596b5d9f15ea2412ccbcd2
Ryan Fitch <ryanfitch@mac.com>
2016-02-04 15:33:16 -0800

    Working through the W3 exercises

---
+++ Python-W3-Exercises/python-exercise37.py
@@ -0,0 +1,9 @@
+# Write a Python program to display your details like name, age,
+# address in three different lines.
+
+name = input("Give me your name: ")
+age = input("Give me your age: ")
+address = input("Give me your address: ")
+print(name)
+print(age)
+print(address)
