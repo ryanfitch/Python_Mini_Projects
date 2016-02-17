4179077dc0f80e8ff00555cdac02e1730184cfe8
Ryan Fitch <ryanfitch@mac.com>
2016-02-17 15:55:48 -0800

    diary app using peewee database

--- treehouse-databases/diary.py
+++ treehouse-databases/diary.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
 import datetime
+import sys
+import os
+
 from peewee import *
 
 db = SqliteDatabase('diary.db')
+
 
 class Entry(Model):
     content = TextField()
@@ -18,17 +23,81 @@ def initialize():
     db.connect()
     db.create_tables([Entry], safe=True)
 
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def menu_loop():
     """Show the menu"""
+    choice = None
+
+    while choice != 'q':
+        clear()
+        print('')
+        print("Enter 'q' to quit.")
+        for key, value in menu.items():
+            print('{}) {}'.format(key, value.__doc__)) # grabs menu description from doc string from each function
+        choice = input('Action: ').lower().strip()
+
+        if choice in menu:
+            clear()
+            menu[choice]()
 
 def add_entry():
     """Add an entry"""
+    clear()
+    print("Enter your entry. Press ctrl+d when finished.")
+    data = sys.stdin.read().strip()
 
-def view_entries():
+    if data:
+        if input('Save entry? [Yn] ').lower() != 'n':
+            Entry.create(content=data)
+            print("Saved successfully!")
+
+
+def view_entries(search_query=None):
     """View previous enties"""
+    entries = Entry.select().order_by(Entry.timestamp.desc())
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
+    for entry in entries:
+        timestamp = entry.timestamp.strftime("%A %B %d %Y %I:%M%p")
+        print(timestamp)
+        print('')
+        print('='*len(timestamp))
+        print('')
+        print(entry.content)
+        print('')
+        print('='*len(timestamp))
+        print('')
+        print('N) next entry')
+        print('d) delete entry')
+        print('q) return to main menu')
+
+        next_action = input('Actions: [Ndq] ').lower().strip()
+        if next_action == 'q':
+            break
+        elif next_action == 'd':
+            delete_entry(entry)
+
+def search_entries():
+    """Search entries for a string."""
+    view_entries(input('Search query:  '))
+
 
 def delete_entry(entry):
     """Delete an entry."""
+    if input("Are you sure? [yN] ").lower() == 'y':
+        entry.delete_instance()
+        print('Entry was deleted')
+
+menu = OrderedDict([
+    ('a', add_entry),
+    ('v', view_entries),
+    ('s', search_entries),
+])
 
 if __name__ == '__main__':
     initialize()
